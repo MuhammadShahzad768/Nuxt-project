ec16276a92a4c92213405af5f805e1a65a43918e
MuhammadShahzad768 <shahzadcannel@gmail.com>
2026-03-02 13:33:16 +0500

    New-content-agencies-

--- pages/privacy-policy.vue
+++ pages/privacy-policy.vue
@@ -38,7 +38,7 @@ import 'swiper/css'
 import 'swiper/css/pagination'
 
 const router = useRouter()
-const pageId = '656'
+const pageId = 'privacy'
 const showLoader = ref(true)
 
 /* =========================
