9987324db5df4f4adae3f5cfd23880f3bc712b4a
MuhammadShahzad768 <shahzadcannel@gmail.com>
2026-03-02 12:05:36 +0500

    show in View:Source

--- pages/content-agencies.vue
+++ pages/content-agencies.vue
@@ -38,7 +38,7 @@ import 'swiper/css'
 import 'swiper/css/pagination'
 
 const router = useRouter()
-const pageId = '656'
+const pageId = '65  6'
 const showLoader = ref(true)
 
 /* =========================
