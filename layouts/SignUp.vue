3d7de7969f1810e3841c57bcc38417a3b2f6d1b5
MuhammadShahzad768 <shahzadcannel@gmail.com>
2026-08-07 10:59:01 +0500

    Trial Page New

---
+++ layouts/SignUp.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Testing from '~/pages/testing.vue';
+
+</script>
+
+
+
+<template>
+  <div class="layout-wrapper overflow-x-hidden">
+    <main class="content">
+      <slot /> 
+    </main>
+  </div>
+</template>
+
+<style scoped>
+.layout-wrapper {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+.content {
+  flex: 1;
+}
+</style>
